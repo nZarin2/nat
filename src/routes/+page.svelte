32a5ce4bbe9d8e800a2eb8a1741ddfc0ae2e00d1
nZarin2 <nzarins@confederationcollege.ca>
2024-03-19 15:07:04 -0400

    d

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<script> import '../style.css'
+let todoItem = '';
+let todoList = [];
+function addToArray() {
+     todoList.push(todoItem);
+     todoList = todoList;
+     console. log(todoList);
+     todoItem= '';
+}
+</script>
+<h1>To do</h1>
+<form on:submit={addToArray}>
+     <input type="text" bind:value={todoItem}>
+<input type="Submit"><button> Add </button>
+</form>
+<ul>
+     {#each todoList as item}
+ <li>
+
+{item}
+
+
+</li>
+
+{/each}
+</ul>
